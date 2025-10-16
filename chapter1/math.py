a66ad8407cfb78e53a0d38a01578c2cdc0b68f23
boss <boss@boss.boss.co>
2025-10-16 22:58:58 +0300

    todays commit

---
+++ chapter1/math.py
@@ -0,0 +1,5 @@
+import math
+pi = 3.14
+print(math.ceil(pi))
+print(pow(pi,2))
+print(math.sqrt(25))
---
+++ chapter1/slicing.py
@@ -0,0 +1,10 @@
+name = "Israel nicodemus"
+first_name = name[0:6]
+print(first_name)
+lastname = name[7:16]
+print(lastname)
+reverse_name = name[::-1]
+print(reverse_name)
+brand = "Mazda demio and axela "
+slice = slice(16,-1)
+print(brand[slice])
